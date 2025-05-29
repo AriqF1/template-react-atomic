5ad057a4e2b90813d1270db3b6647395a8235200
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-29 11:21:27 +0700

    add PostCSS configuration for Tailwind CSS and Autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+// This file is used to configure PostCSS with Tailwind CSS and Autoprefixer.
+// It ensures that Tailwind CSS styles are processed correctly and that vendor prefixes are added where necessary.
