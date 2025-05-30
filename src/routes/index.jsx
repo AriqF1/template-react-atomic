952b9abbf668d9ee1d7de602917182f315b6a140
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-30 13:27:03 +0700

    add: implement routing for NotFoundPage in Router component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../components/templates/MainLayout";
 import HomePage from "../components/pages/HomePage";
+import NotFoundPage from "../components/pages/NotFoundPage";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout><HomePage /></MainLayout>} />
+      <Route
+        path="/"
+        element={
+          <MainLayout>
+            <HomePage />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <MainLayout>
+            <NotFoundPage />
+          </MainLayout>
+        }
+      />
     </Routes>
   );
 };
