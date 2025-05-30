76708bb5ba038915452f9f1bc2a55dadbe03a3bd
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-30 13:10:26 +0700

    refactor: restructure MainLayout to include Footer and improve layout styling

--- src/components/templates/MainLayout.jsx
+++ src/components/templates/MainLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../organisms/Header";
+import Footer from "../organisms/Footer";
 
 const MainLayout = ({ children }) => {
   return (
     <>
-      <Header />
-      <main className="p-6">{children}</main>
+      <div className="bg-gray-100 min-h-screen flex flex-col">
+        <Header />
+        <main className="p-6 flex-grow">{children}</main>
+        <Footer />
+      </div>
     </>
   );
 };
