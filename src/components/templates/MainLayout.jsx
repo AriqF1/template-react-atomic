a5b2e0fe529872dea07d37a1743f3bc46a2c7be3
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-30 13:26:55 +0700

    style: remove padding from MainLayout main section for consistent layout

--- src/components/templates/MainLayout.jsx
+++ src/components/templates/MainLayout.jsx
@@ -7,7 +7,7 @@ const MainLayout = ({ children }) => {
     <>
       <div className="bg-gray-100 min-h-screen flex flex-col">
         <Header />
-        <main className="p-6 flex-grow">{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </div>
     </>
