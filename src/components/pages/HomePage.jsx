2d8a219f6a549c6124f309e9f9a26e58c9268733
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-02 22:24:39 +0700

    fix: update HomePage layout and input placeholders for clarity

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,12 +1,16 @@
-import React from "react";
 import Button from "../atoms/Button";
 import InputWithLabel from "../molecules/InputWithLabel";
 
 const HomePage = () => {
   return (
-    <div className="container mx-auto p-6 bg-white shadow-md rounded-lg">
+    <div className="h-screen flex flex-col items-center justify-center text-center">
       <h2 className="text-2xl font-bold mb-4">Welcome to Home Page</h2>
-      <InputWithLabel label="Name" id="name" placeholder="Your name" />
+      <InputWithLabel label="Name" id="name" placeholder="Type Your Name..." />
+      <InputWithLabel
+        label="Password"
+        id="password"
+        placeholder="Type Your Password..."
+      />
       <Button label="Submit" />
     </div>
   );
